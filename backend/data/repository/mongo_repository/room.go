a5b5973c1a9d39bd493822347881788491c7e7a3
Rodchananat K <rod8711@gmail.com>
2020-12-11 17:09:27 +0700

    fix adding room

--- backend/data/repository/mongo_repository/room.go
+++ backend/data/repository/mongo_repository/room.go
@@ -4,6 +4,7 @@ import (
 	"backendSenior/domain/interface/repository"
 	"backendSenior/domain/model"
 	"backendSenior/utills"
+	"fmt"
 	"time"
 
 	"github.com/globalsign/mgo/bson"
@@ -65,29 +66,41 @@ func (roomMongo RoomRepositoryMongo) DeleteRoomByID(roomID string) error {
 // AddMemberToRoom appends member ids to room, return errors when it doesn't exists
 func (roomMongo RoomRepositoryMongo) AddMemberToRoom(roomID string, listUser []string) error {
 	// TODO might need to fix logic
-	err := roomMongo.ConnectionDB.DB(dbName).C(collectionRoom).UpdateId(roomID, bson.M{
+	fmt.Println(roomID)
+	bid := bson.ObjectIdHex(roomID)
+	fmt.Println(bid)
+	err := roomMongo.ConnectionDB.DB(dbName).C(collectionRoom).UpdateId(bid, bson.M{
 		"$push": bson.M{
 			"listUser": bson.M{
-				"$each": listUser, // add all from listUser to array
+				"$each": toObjectIdArr(listUser), // add all from listUser to array
 			},
 		},
 	})
 	if err != nil {
-		return err
+		return fmt.Errorf("error updating room users %s", err)
 	}
 
 	var room model.Room
-	err = roomMongo.ConnectionDB.DB(dbName).C(collectionRoom).FindId(roomID).One(&room)
+	err = roomMongo.ConnectionDB.DB(dbName).C(collectionRoom).FindId(bson.ObjectIdHex(roomID)).One(&room)
 	if err != nil {
-		return err
+		return fmt.Errorf("error re-getting room users %s", err)
 	}
 
 	for _, s := range room.ListUser {
 		var user model.User
-		err = roomMongo.ConnectionDB.DB("User").C("UserData").FindId(s).One(&user)
-		newUser := bson.M{"$set": bson.M{"room": append(user.Room, bson.ObjectId(roomID))}}
+		fmt.Println("user", s)
+		err = roomMongo.ConnectionDB.DB(dbName).C(collectionUser).FindId(s).One(&user)
+		if err != nil {
+			err = fmt.Errorf("error finding user %s", err)
+			break
+		}
+		newUser := bson.M{"$set": bson.M{"room": append(user.Room, bson.ObjectIdHex(roomID))}}
 		userID := user.UserID
-		err = roomMongo.ConnectionDB.DB("User").C("UserData").UpdateId(userID, newUser)
+		err = roomMongo.ConnectionDB.DB(dbName).C(collectionUser).UpdateId(userID, newUser)
+		if err != nil {
+			err = fmt.Errorf("error updating user %s", err)
+			break
+		}
 	}
 	return err
 }
