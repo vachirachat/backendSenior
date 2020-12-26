fd6f5e29817faa3996bea8af8307ca558bc00c1e
Rodchananat K <rod8711@gmail.com>
2020-12-26 12:38:59 +0000

    fix lowercase header

--- backend/controller/middleware/auth/helper.go
+++ backend/controller/middleware/auth/helper.go
@@ -10,6 +10,13 @@ import (
 func extractToken(context *gin.Context) string {
 	// Get JWT from Header Authorization
 	bearToken := context.Request.Header.Get("Authorization")
+	if bearToken == "" {
+		v := context.Request.Header["authorization"]
+		if len(v) !=  0 {
+			bearToken = v[0]
+		}
+	}
+
 	strArr := strings.Split(bearToken, " ")
 	if len(strArr) == 2 {
 		return strArr[1]
@@ -20,6 +27,13 @@ func extractToken(context *gin.Context) string {
 func extractBasicHeader(context *gin.Context) (string, string) {
 	// Get JWT from Header Authorization
 	basicToken := context.Request.Header.Get("Authorization")
+	if basicToken == "" {
+	        v := context.Request.Header["authorization"]
+                if len(v) !=  0 {
+                        basicToken = v[0]
+                }
+	}
+
 	strArr := strings.Split(basicToken, " ")
 	if len(strArr) == 2 {
 		userPass, err := base64.StdEncoding.DecodeString(strArr[1])
--- proxy-server/controller/middleware/helper.go
+++ proxy-server/controller/middleware/helper.go
@@ -9,6 +9,12 @@ import (
 func extractToken(context *gin.Context) string {
 	// Get JWT from Header Authorization
 	bearToken := context.Request.Header.Get("Authorization")
+	if bearToken == "" {
+		v := context.Request.Header["authorization"]
+		if len(v) != 0 {
+			bearToken = v[0]
+		}
+	}
 	strArr := strings.Split(bearToken, " ")
 	if len(strArr) == 2 {
 		return strArr[1]
