d9a035417904a6ac1edd1dbc52e60e4a5062d93b
Waritphon <waritphon_sriphrom@outlook.co.th>
2021-01-04 23:06:16 +0700

    refactor en/decrypted Message

--- proxy-server/domain/service/encryption.go
+++ proxy-server/domain/service/encryption.go
@@ -11,6 +11,7 @@ import (
 	"fmt"
 	"io"
 	"log"
+	model_proxy "proxySenior/domain/model"
 	"time"
 
 	"github.com/mergermarket/go-pkcs7"
@@ -35,7 +36,6 @@ func NewEncryptionService(keystore repository.Keystore) *EncryptionService {
 
 // Encrypt takes a message, then return message with data encrypted
 func (enc *EncryptionService) Encrypt(message model.Message) (model.Message, error) {
-	log.Println("\n Encrypted log.Println(keyRec.KeyRecodes --> \n")
 	keyRec, err := enc.keystore.GetKeyForMessage(message.RoomID.Hex(), message.TimeStamp)
 
 	if err != nil {
@@ -43,90 +43,62 @@ func (enc *EncryptionService) Encrypt(message model.Message) (model.Message, err
 		if err != nil {
 			return message, fmt.Errorf("Fail to Add Key: %s", err.Error())
 		}
-	} else {
+	}
+
+	loc, err := time.LoadLocation(utils.BACKKOKTIMEZONE)
+	keyTime, err := validateTime(keyRec.KeyRecodes[len(keyRec.KeyRecodes)-1])
+	if err != nil {
+		return message, fmt.Errorf("Fail to Add Change TimeZone: %s", err.Error())
+	}
+
+	if keyTime.ValidTo.Before(time.Now().In(loc)) {
 		keyRec, err = enc.keystore.UpdateNewKey(message.RoomID.Hex(), keyRec.KeyRecodes, message.TimeStamp)
 		if err != nil {
 			return message, fmt.Errorf("Fail to Update Key: %s", err.Error())
 		}
 	}
-
-	//For Test Propose
-	for i := range keyRec.KeyRecodes {
-		log.Print(keyRec.KeyRecodes[i].Key)
-	}
-
-	key := keyRec.KeyRecodes[len(keyRec.KeyRecodes)-1].Key
-
-	plainText := []byte(message.Data)
-	plainText, err = pkcs7.Pad(plainText, aes.BlockSize)
-	if err != nil {
-		return message, fmt.Errorf("padding plaintext: %s", err.Error())
-	}
-
-	if len(plainText)%aes.BlockSize != 0 {
-		return message, fmt.Errorf("padding error: wrong size")
-	}
-
-	block, err := aes.NewCipher(key)
-	if err != nil {
-		return message, err
-	}
-
-	cipherText := make([]byte, len(plainText)+aes.BlockSize)
-	iv := cipherText[:aes.BlockSize]
-	_, err = io.ReadFull(rand.Reader, iv)
-	if err != nil {
-		return message, fmt.Errorf("error init iv: %s", err.Error())
-	}
-
-	mode := cipher.NewCBCEncrypter(block, iv)
-	mode.CryptBlocks(cipherText[aes.BlockSize:], plainText)
-
-	// encode base 64 before send
-	var result bytes.Buffer
-	b64 := base64.NewEncoder(base64.StdEncoding, &result)
-	b64.Write(cipherText)
-	b64.Close()
-
-	message.Data = result.String()
-
-	return message, nil
+	return encryptedMessage(message, keyRec.KeyRecodes[len(keyRec.KeyRecodes)-1].Key)
 }
 
 // Decrypt takes a message, then return message with data decrypted with appropiate key
 func (enc *EncryptionService) Decrypt(message model.Message) (model.Message, error) {
-	log.Println("\n Decrypted log.Println(keyRec.KeyRecodes --> \n")
-	// fmt.Printf("[Decode] original message text is [%s]\n", message.Data)
-	// decoder := base64.NewDecoder(base64.StdEncoding, bytes.NewReader([]byte(message.Data)))
-	// decoded, err := ioutil.ReadAll(decoder)
-	// if err != nil {
-	// 	message.Data = "Error Decoding: " + err.Error()
-	// } else {
-	// 	message.Data = string(decoded)
-	// }
 	keyRec, err := enc.keystore.GetKeyForMessage(message.RoomID.Hex(), message.TimeStamp)
-	//----- Refactor to Function Check key-date -----//
-
 	if err != nil {
 		return message, fmt.Errorf("getting key: %s", err.Error())
 	}
-	//----- TODO :: Refactor to Function -----//
-	//----- Refactor to Function ReMapTime -----//
+	key, err := decryptedKeyValidate(keyRec, message)
+	if err != nil {
+		return message, fmt.Errorf("Fail to Decrypt: %s", err.Error())
+	}
+	return decryptedMessage(message, key)
+}
+
+func validateTime(keyRec model_proxy.KeyRecord) (model_proxy.KeyRecord, error) {
+	loc, err := time.LoadLocation(utils.BACKKOKTIMEZONE)
+	keyRec.ValidTo = keyRec.ValidTo.In(loc)
+	keyRec.ValidFrom = keyRec.ValidFrom.In(loc)
+	return keyRec, err
+}
+
+// validate key by time
+func decryptedKeyValidate(keyRec model_proxy.RoomKeys, message model.Message) ([]byte, error) {
 	loc, err := time.LoadLocation(utils.BACKKOKTIMEZONE)
 	key := keyRec.KeyRecodes[0].Key
 	for i := range keyRec.KeyRecodes {
-		keyMap := keyRec.KeyRecodes[i]
-		keyMap.ValidTo = keyMap.ValidTo.In(loc)
-		keyMap.ValidFrom = keyMap.ValidFrom.In(loc)
+		keyMap, err := validateTime(keyRec.KeyRecodes[i])
+		if err != nil {
+			return make([]byte, 0), fmt.Errorf("Fail to Add Change TimeZone: %s", err.Error())
+		}
 		if keyMap.ValidTo.After(message.TimeStamp.In(loc)) && keyMap.ValidFrom.Before(message.TimeStamp.In(loc)) {
 			key = keyMap.Key
 			log.Print("Match key >>> ", key)
-			continue
+			return key, nil
 		}
 	}
-	//----- Refactor to Function ReMapTime -----//
+	return make([]byte, 0), fmt.Errorf("Key not Found : %s", err.Error())
+}
 
-	// b64 := base64.NewDecoder(base64.StdEncoding, bytes.NewReader([]byte(message.Data)))
+func decryptedMessage(message model.Message, key []byte) (model.Message, error) {
 	cipherText, err := base64.StdEncoding.DecodeString(message.Data)
 	if err != nil {
 		return message, fmt.Errorf("decode b64: %s", err.Error())
@@ -158,6 +130,41 @@ func (enc *EncryptionService) Decrypt(message model.Message) (model.Message, err
 
 	decrypted, _ = pkcs7.Unpad(decrypted, aes.BlockSize)
 	message.Data = string(decrypted)
+	return message, nil
+}
 
+func encryptedMessage(message model.Message, key []byte) (model.Message, error) {
+	plainText := []byte(message.Data)
+	plainText, err := pkcs7.Pad(plainText, aes.BlockSize)
+	if err != nil {
+		return message, fmt.Errorf("padding plaintext: %s", err.Error())
+	}
+
+	if len(plainText)%aes.BlockSize != 0 {
+		return message, fmt.Errorf("padding error: wrong size")
+	}
+
+	block, err := aes.NewCipher(key)
+	if err != nil {
+		return message, err
+	}
+
+	cipherText := make([]byte, len(plainText)+aes.BlockSize)
+	iv := cipherText[:aes.BlockSize]
+	_, err = io.ReadFull(rand.Reader, iv)
+	if err != nil {
+		return message, fmt.Errorf("error init iv: %s", err.Error())
+	}
+
+	mode := cipher.NewCBCEncrypter(block, iv)
+	mode.CryptBlocks(cipherText[aes.BlockSize:], plainText)
+
+	// encode base 64 before send
+	var result bytes.Buffer
+	b64 := base64.NewEncoder(base64.StdEncoding, &result)
+	b64.Write(cipherText)
+	b64.Close()
+
+	message.Data = result.String()
 	return message, nil
 }
--- proxy-server/go.mod
+++ proxy-server/go.mod
@@ -6,6 +6,7 @@ replace backendSenior => ../backend
 
 require (
 	backendSenior v0.0.0-00010101000000-000000000000
+	github.com/Workiva/go-datastructures v1.0.52
 	github.com/gin-gonic/gin v1.3.0
 	github.com/globalsign/mgo v0.0.0-20181015135952-eeefdecb41b8
 	github.com/gorilla/websocket v1.4.2
